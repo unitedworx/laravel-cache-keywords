4dbca1de2d0307119af41512f123eb73e85b8ec2
Propaganistas <propaganistas@gmail.com>
2015-10-08 11:31:17 +0200

    Extend CacheServiceProvider (fixes #2)

--- src/CacheKeywordsServiceProvider.php
+++ src/CacheKeywordsServiceProvider.php
@@ -1,15 +1,35 @@
-<?php namespace Propaganistas\LaravelCacheKeywords;
+<?php
 
-use Illuminate\Support\ServiceProvider as IServiceProvider;
+/**
+ * This Service Provider extends Laravel's built-in CacheServiceProvider class on a very specific purpose.
+ * In fact we're actually overriding ALL bindings registered in CacheServiceProvider to be handled
+ * through this class (although most of them are transparently passed to CacheServiceProvider).
+ * Why? Because Artisan loads all deferred providers at once using a key-sorted providers array:
+ *
+ *  ...
+ *  "cache" => CacheKeywordsServiceProvider,
+ *  "cache.store" => CacheServiceProvider,
+ *  ...
+ *
+ * As you can see, the cache.store binding calls the built-in CacheServiceProvider afterwards and hence renders
+ * our binding useless if we would only bind our override. So we are obliged to convert the providers array to:
+ *
+ * ...
+ *  "cache" => CacheKeywordsServiceProvider,
+ *  "cache.store" => CacheKeywordsServiceProvider,
+ *  ...
+ *
+ * in order to have our override resolve correctly.
+ *
+ * It's sort of a hack, but it's the only way to make it work across the entire Application.
+ */
 
-class CacheKeywordsServiceProvider extends IServiceProvider
+namespace Propaganistas\LaravelCacheKeywords;
+
+use Illuminate\Cache\CacheServiceProvider;
+
+class CacheKeywordsServiceProvider extends CacheServiceProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
 
     /**
      * Register the service provider.
@@ -18,19 +38,13 @@ class CacheKeywordsServiceProvider extends IServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(['cache', 'Illuminate\Cache\CacheManager', 'Illuminate\Contracts\Cache\Factory'], function ($app) {
+        // Override CacheServiceProvider bindings to use this Service Provider transparently.
+        parent::register();
+
+        // Override the 'cache' binding to our CacheManager.
+        $this->app->singleton('cache', function ($app) {
             return new CacheManager($app);
         });
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return ['cache', 'Illuminate\Cache\CacheManager', 'Illuminate\Contracts\Cache\Factory'];
     }
 
 }
