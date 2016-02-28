2010f3907366d7e2814ff659f64577ed202a5a46
Propaganistas <Propaganistas@users.noreply.github.com>
2016-02-28 10:50:05 +0100

    Update CacheKeywordsServiceProvider.php

--- src/CacheKeywordsServiceProvider.php
+++ src/CacheKeywordsServiceProvider.php
@@ -12,9 +12,6 @@ class CacheKeywordsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Override CacheServiceProvider bindings to use this Service Provider transparently.
-        parent::register();
-
         // Override the 'cache' binding to our CacheManager.
         $this->app->extend('cache', function ($original, $app) {
             return new CacheManager($app);
